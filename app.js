5dfbcf6f416e8d5c3d1759e72bfd56cdfdf07199
devonhackley <devonhackley@gmail.com>
2016-10-20 16:33:18 -0700

    Function for first five question done

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 'use strict';
 
-var quiz1 = prompt('Is Christina terrified of small breed horses?');
+var answerListing = ['yes','no','yes','no','yes'];
+var questionListing = ['Is Christina terrified of small breed horses?','Is Christina\'s favorite medium acrylics?','Has Christina climbed Mount Huashan?','Can Christina play the guitar?','Does Christina occasionally dream of being a storm chaser?'];
+
+
+
+//functions
+function quiz() {
+  for (var i = 0; i < questionListing.length; i++){
+    var answer = prompt(questionListing[i]);
+    for (var j = 0; j < answerListing.length; j++){
+      if (questionListing[i] === answerListing[j]){
+        alert('Correct');
+      }else {
+        alert('Fail');
+      }
+    }
+  }
+  if (toLowerCase() === 'yes') {
+    alert('To a degree, note we didn\'t call them ponies. +1');
+  }
+  else if (quiz1.toLowerCase() === 'no') {
+    alert('Sadly, she is. -1');
+  }
+  else {
+    alert('"Yes" or "No"');
+  }
+}
+
 console.log('quiz1: ', quiz1);
 console.log(quiz1.toLowerCase());
-console.log(quiz1);
-if (quiz1.toLowerCase() === 'yes') {
-  alert('To a degree, note we didn\'t call them ponies. +1');
-}
-else if (quiz1.toLowerCase() === 'no') {
-  alert('Sadly, she is. -1');
-}
-else {
-  alert('"Yes" or "No"');
-}
 
 var quiz2 = prompt('Is Christina\'s favorite medium acrylics?');
 console.log('quiz2:', quiz2);
@@ -27,7 +44,7 @@ else {
   alert('"Yes" or "No"');
 }
 
-var quiz3 = prompt('Has Christina climbed Mount Huashan?');
+
 console.log('quiz3:', quiz3);
 console.log(quiz3.toLowerCase());
 if (quiz3.toLowerCase === 'yes') {
@@ -39,7 +56,7 @@ else if (quiz3.toLowerCase() === 'no') {
 else {
   alert('"Yes" or "No"');
 }
-var quiz4 = prompt('Can Christina play the guitar?');
+
 console.log('quiz4', quiz3);
 if (quiz4.toLowerCase() === 'no') {
   alert('Correct! +1');
@@ -61,3 +78,12 @@ else if (quiz5 === 'no') {
 else {
   alert('"Yes" or "No"');
 }
+
+
+
+//Do stuff
+var quiz1 = prompt('Is Christina terrified of small breed horses?');
+var quiz2 = prompt('Is Christina\'s favorite medium acrylics?');
+var quiz3 = prompt('Has Christina climbed Mount Huashan?');
+var quiz4 = prompt('Can Christina play the guitar?');
+var quiz5 = prompt('Does Christina occasionally dream of being a storm chaser?');
